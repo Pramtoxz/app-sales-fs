24486eb5a5474bab27dd926b648de79d02a6560c
Pramtoxz <pramuditometra@gmail.com>
2026-07-29 14:17:47 +0700

    fix: import collection

--- app/Exports/TargetDealerTemplateExport.php
+++ app/Exports/TargetDealerTemplateExport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
